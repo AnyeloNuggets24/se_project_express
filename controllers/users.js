c8a11301501d7c827567f5d2e89871fbf80518bc
ANYELO CARRANZE <carrnzabeast3@gmail.com>
2026-02-14 00:33:40 -0500

    sp.12

---
+++ controllers/users.js
@@ -0,0 +1,17 @@
+const e = require("express");
+const User = require("../models/clothingItems");
+
+// GET /users
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = {
+  getUsers,
+};
